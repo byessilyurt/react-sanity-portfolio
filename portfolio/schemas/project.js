8f2236cff2c51b91d51dc25059b7da6c70f77b34
yessilyurt <y.yesilyurt14@gmail.com>
2022-06-11 00:15:50 +0100

    data source migrated to sanity

---
+++ portfolio/schemas/project.js
@@ -0,0 +1,46 @@
+export default {
+  name: "project",
+  title: "Project",
+  type: "document",
+  fields: [
+    {
+      name: "name",
+      title: "Name",
+      type: "string",
+    },
+    {
+      name: "tag",
+      title: "tag",
+      type: "array",
+      of: [
+        {
+          name: "name",
+          title: "Name",
+          type: "string",
+        },
+      ],
+      // set a many to many relationship with the category type
+    },
+    {
+      name: "description",
+      title: "Description",
+      type: "string",
+      // convert the description to block content (rich text)
+    },
+    {
+      name: "imageURL",
+      title: "Image URL",
+      type: "string",
+    },
+    {
+      name: "demoURL",
+      title: "Demo URL",
+      type: "string",
+    },
+    {
+      name: "githubURL",
+      title: "GitHub URL",
+      type: "string",
+    },
+  ],
+};
